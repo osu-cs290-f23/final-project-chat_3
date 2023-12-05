aa2cfcab7f5a609dbeaaab88d880d08d551be8b2
Diego <diazdiad@oregonstate.edu>
2023-12-04 16:52:57 -0800

    made sent and received messages different

--- script.js
+++ script.js
@@ -1,55 +1,114 @@
-
 const socket = io('http://localhost:3000');
-const messageForm = document.getElementById('send-window'); // Change ID to match your HTML
+const messageForm = document.getElementById('send-window');
 const messageInput = document.getElementById('message-input');
 const messageContainer = document.getElementById('message-container');
 const darkModeToggle = document.querySelector('.dark-mode-toggle');
-const userNameElement = document.getElementById('user-name'); // Added this line
+const userNameElement = document.getElementById('user-name');
 
 let userName = prompt('What is your name?');
 if (!userName) userName = "Anonymous";
 
-userNameElement.innerText = `User: ${userName}`; // Added this line
+userNameElement.innerText = `Chatting with: Waiting for user...`;
+
+socket.emit('new-user', userName);
+
+socket.on('existing-users', usersList => {
+  if (usersList.length > 0) {
+    userNameElement.innerText = `Chatting with: ${usersList[0]}`;
+  }
+});
+
+socket.on('new-user-joined', otherUserName => {
+  userNameElement.innerText = `Chatting with: ${otherUserName}`;
+});
+
+socket.on('send-chat-message', data => {
+  appendMessage(data.message, true, data.sender);
+});
+
+socket.on('chat-message', data => {
+  appendMessage(data.message, false, data.sender);
+});
+
+socket.on('user-connected', userName => {
+  appendMessage(`${userName} joined`, false, 'System');
+});
+
+socket.on('user-disconnected', userName => {
+  appendMessage(`${userName} disconnected`, false, 'System');
+});
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = messageInput.value;
+  appendMessage(`You: ${message}`, true, userName);
+  socket.emit('send-chat-message', { message, sender: userName });
+  messageInput.value = '';
+});
+
+
+function appendMessage(message, isSent, sender) {
+  const messageElement = document.createElement('div');
+  const senderText = isSent ? '' : (sender ? `${sender}: ` : '');
+
+  messageElement.innerText = `${senderText}${message}`;
+  messageElement.classList.add('message', isSent ? 'sent' : 'received');
+
+  messageContainer.append(messageElement);
+
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+// socket.emit('send-chat-message', { message, sender: userName });
+
+
+
+
+
+// Add an event listener for the 'input' event on the message input field
+messageInput.addEventListener('input', function() {
+  // Set the height of the input field based on its scrollHeight
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+});
+
+// Add an event listener for the 'keypress' event to prevent the Enter key from creating a new line
+messageInput.addEventListener('keypress', function(e) {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    // Optionally, add logic to handle sending the message on Enter
+    sendMessage();
+  }
+});
+
+// Function to handle sending the message (you can adjust this based on your needs)
+function sendMessage() {
+  const message = messageInput.value;
+  if (message.trim() !== '') {
+    appendMessage(`You: ${message}`, true, 'You');
+    socket.emit('send-chat-message', { message, sender: 'You' });
+    messageInput.value = '';
+    // Reset the input field height
+    messageInput.style.height = 'auto';
+  }
+}
+
+
+
+
+
+
+
+
+
 
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
   messageForm.classList.toggle('dark-mode');
   messageInput.classList.toggle('dark-mode-input');
+  messageContainer.classList.toggle('dark-mode'); // Add this line to toggle dark mode for the message container
   messageContainer.scrollTop = messageContainer.scrollHeight;
-}
-
-appendMessage('You joined');
-socket.emit('new-user', userName);
-
-socket.on('chat-message', data => {
-  console.log(`Message received: ${data.name}: ${data.message}`);
-  appendMessage(`${data.name}: ${data.message}`);
-});
-
-
-
-socket.on('user-connected', userName => {
-  appendMessage(`${userName} joined`);
-});
-
-socket.on('user-disconnected', userName => {
-  appendMessage(`${userName} disconnected`);
-});
-
-messageForm.addEventListener('submit', e => {
-  e.preventDefault(); // Prevent form submission from reloading the page
-  const message = messageInput.value;
-  appendMessage(`You: ${message}`);
-  socket.emit('send-chat-message', message);
-  messageInput.value = '';
-});
-
-function appendMessage(message) {
-  const messageElement = document.createElement('div');
-  messageElement.innerText = message;
-  messageContainer.append(messageElement);
-
-  // Automatically scroll down to the latest message
-  messageContainer.scrollTop = messageContainer.scrollHeight;
+  const menuBar = document.getElementById('menu-bar');
+  menuBar.classList.toggle('dark-mode');
 }
 
